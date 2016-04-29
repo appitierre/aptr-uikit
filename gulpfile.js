f37bbbeef6b7d9f9d7887cbe4aaa9546f95eb672
Daryl Hedley <darylhedley@hotmail.com>
2016-04-29 14:58:22 +0100

    updated publish and dist less

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,7 @@ function lessBuild(isProduction, callback) {
     gulp.src(['src/less/main.less'])
         .pipe(sourcemaps.init())
         .pipe(less())
-        .pipe(rename("aptr-uikit.less"))
+        .pipe(rename("aptr-uikit.css"))
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./build/css'));
     
@@ -91,9 +91,9 @@ function lessDist(isProduction, callback) {
     gulp.src(['src/uiElements/less/main.less'])
         .pipe(sourcemaps.init())
         .pipe(less())
-        .pipe(rename("aptr-uikit.css"))
+        .pipe(rename("aptr-uikit.less"))
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./dist/less'));
     
     callback();
 }
