8cf2a8f991e323c34d5ba524dad9de0dd09ed0ce
Daryl Hedley <darylhedley@hotmail.com>
2016-04-29 15:50:43 +0100

    Now includes Button and FlatButton

--- src/uiElements/index.js
+++ src/uiElements/index.js
@@ -1,5 +1,9 @@
+var Button = require('./components/buttons/button');
+var FlatButton = require('./components/buttons/flatButton');
 var CollectionItem = require('./components/collection/collectionItem');
 
 module.exports = {
+    Button: Button,
+    FlatButton: FlatButton,
     CollectionItem: CollectionItem
 }
