5b8f774f5782e4df47469cfd4a12a30456449733
brandonk1234 <BrandonKarunakaran@Brandons-MacBook-Pro.local>
2016-05-20 11:28:52 +0100

    Markup and functionality of dropDown component working

--- src/uiElements/components/dropDown/dropDownOptions.jsx
+++ src/uiElements/components/dropDown/dropDownOptions.jsx
@@ -1,23 +1,50 @@
 var React = require('react');
+var Button = require('../buttons/button'); 
 
 var DropDownOptions = React.createClass({
 
+
+	getInitialState: function() {
+		return {
+			isDisplayingList: false
+		}
+	},
+	
 	getItems: function() {
-		return _.map(this.props.options, function(item, index){
-			return (
-				<div className="drop-down-items">
-					{item.text}
-				</div>
-			)
-		})
+		if (this.state.isDisplayingList === true) {	
+			return _.map(this.props.options, function(item, index){
+				return (
+					<div className="drop-down-item">
+						{item.text}
+					</div>
+				)
+			})
+		}
+	},
+
+	getSelector: function() {
+		return (
+			<div className="drop-down-selector">
+				<Button type="primary" text="select" onClick={this.onSelectorClicked}/>
+			</div> 
+		)
+	},
+
+	onSelectorClicked: function() {
+		 var isDisplayingList = !this.state.isDisplayingList;
+        
+        this.setState({
+            isDisplayingList: isDisplayingList
+        });
 	},
 
 	render: function() {
 		return (
 			<div className="drop-down">
-				<div className="drop-down-selector">
+				{this.getSelector()}
+				<div className="drop-down-item-container">	
+					{this.getItems()}				
 				</div>
-				{this.getItems()}				
 			</div>
 		);
 	}
