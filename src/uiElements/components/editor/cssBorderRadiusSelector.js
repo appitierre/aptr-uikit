590ca0ff5f388cd291f544cb3d580049566eeb56
brandonk1234 <BrandonKarunakaran@Brandons-MacBook-Pro.local>
2016-07-20 12:25:25 +0100

    Styling for border-selector-radius component complete

--- src/uiElements/components/editor/cssBorderRadiusSelector.js
+++ src/uiElements/components/editor/cssBorderRadiusSelector.js
@@ -1,37 +1,75 @@
 var React = require('react');
+var classnames = require('classnames');
 var Button = require('../buttons/button');
 
 var CssBorderRadiusSelector = React.createClass({
 
+	getClassName: function() {
+		if (this.props.topLeft && this.props.topRight && this.props.bottomRight && this.props.bottomLeft) {
+			return {
+				topLeft: 'button css-border-radius-selector-button css-border-radius-selector-button-top-left',
+				topRight: 'button css-border-radius-selector-button css-border-radius-selector-button-top-right',
+				bottomRight: 'button css-border-radius-selector-button css-border-radius-selector-button-bottom-right',
+				bottomLeft: 'button css-border-radius-selector-button css-border-radius-selector-button-bottom-left',
+				center: 'button css-border-radius-selector-button css-border-radius-selector-button-center primary' 
+			}
+		} 
+
+		return {
+			topLeft: classnames({'button css-border-radius-selector-button css-border-radius-selector-button-top-left primary': (this.props.topLeft)}, 
+				{'button css-border-radius-selector-button css-border-radius-selector-button-top-left': (!this.props.topLeft)}),
+			
+			topRight: classnames({'button css-border-radius-selector-button css-border-radius-selector-button-top-right primary': (this.props.topRight)},
+				{'button css-border-radius-selector-button css-border-radius-selector-button-top-right': !this.props.topRight}),
+			
+			bottomRight: classnames({'button css-border-radius-selector-button css-border-radius-selector-button-bottom-right primary': (this.props.bottomRight)},
+				{'button css-border-radius-selector-button css-border-radius-selector-button-bottom-right': !this.props.bottomRight}),
+			
+			bottomLeft: classnames({'button css-border-radius-selector-button css-border-radius-selector-button-bottom-left primary': (this.props.bottomLeft)},
+				{'button css-border-radius-selector-button css-border-radius-selector-button-bottom-left': !this.props.bottomLeft}),
+			
+			center: classnames({'button css-border-radius-selector-button css-border-radius-selector-button-center': !this.props.center}),
+		}
+	},	
+
 	renderItems: function() {
 		return (
 			<div className="css-border-radius-selector">
-				<div className="css-border-radius-selector-top">
 					<button 
-						className="button css-border-radius-selector-top-left-button"
+						className={this.getClassName().topLeft}
 						onClick={this.onTopLeftButtonClicked}>
+						<span className="css-border-radius-selector-button-icon-top-left">
+						</span>
 					</button>
 					<button 
-						className="button css-border-radius-selector-top-right-button"
+						className={this.getClassName().topRight}
 						onClick={this.onTopRightButtonClicked}>
+						<span className="css-border-radius-selector-button-icon-top-right">
+						</span>
 					</button> 
-				</div>
-				<div className="css-border-radius-selector-centre">
+				
+				
 					<button
-						className="button css-border-radius-selector-centre-button"
+						className={this.getClassName().center}
 						onClick={this.onCentreButtonClicked}>
+						<span className="css-border-radius-selector-button-icon-center">
+						</span>
 					</button>
-				</div>
-				<div className="css-border-radius-selector-bottom">
+				
+				
 					<button 
-						className="button css-border-radius-selector-bottom-left-button"
+						className={this.getClassName().bottomLeft}
 						onClick={this.onBottomLeftClicked}>
+						<span className="css-border-radius-selector-button-icon-bottom-left">
+						</span>
 					</button>
 					<button 
-						className="button css-border-radius-selector-bottom-right-button"
+						className={this.getClassName().bottomRight}
 						onClick={this.onBottomRightClicked}>
+						<span className="css-border-radius-selector-button-icon-bottom-right">
+						</span>
 					</button>
-				</div>
+				
 			</div>
 		);
 	},
