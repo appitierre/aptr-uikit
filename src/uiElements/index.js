f5890567a7aa53a0d391de1f2ec4feb923af59e2
Daryl Hedley <darylhedley@hotmail.com>
2016-08-26 12:02:40 +0100

    Added border radius selector

--- src/uiElements/index.js
+++ src/uiElements/index.js
@@ -21,6 +21,7 @@ var CheckboxItem = require('./components/checkboxItem/checkboxItem');
 var Message = require('./components/message/message');
 var CssValue = require('./components/cssValue/cssValue');
 var CssBorderSelector = require('./components/cssBorderSelector/cssBorderSelector');
+var CssBorderRadiusSelector = require('./components/cssBorderRadiusSelector/cssBorderRadiusSelector');
 var StarRating = require('./components/starRating/starRating')
 
 module.exports = {
