bc920aac893cd84537b51f6cbf61f9b23fb92687
brandonk1234 <BrandonKarunakaran@Brandons-MacBook-Pro.local>
2016-11-24 09:57:48 +0000

    removed less nesting and renamed function

--- src/uiElements/accordion/components/accordion.js
+++ src/uiElements/accordion/components/accordion.js
@@ -3,7 +3,7 @@ var AccordionItem = require('./accordionItem');
 
 var Accordion = React.createClass({
 
-	getComponents: function() {
+	getItems: function() {
 
 		return _.map(this.props.items, (item, index) => {
 			var iconSelected = this.props.icon;
@@ -27,7 +27,7 @@ var Accordion = React.createClass({
 	render: function() {
 		return (
 			<div className="accordion">
-				{this.getComponents()}
+				{this.getItems()}
 			</div>
 		);
 	}
