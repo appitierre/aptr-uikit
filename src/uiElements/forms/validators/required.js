3879cbb30e1a271dfc7b1fb8df2da5a9d0165b60
Daryl Hedley <darylhedley@hotmail.com>
2017-02-08 13:17:37 +0000

    Fixed validators for numbers

--- src/uiElements/forms/validators/required.js
+++ src/uiElements/forms/validators/required.js
@@ -1,7 +1,7 @@
 var registerValidator = require('../helpers').registerValidator;
 
 var required = function(model, attribute, value) {
-    if (value.length) {
+    if (value.length || (typeof value === 'number' && (value % 1) === 0)) {
         return false;
     } else {
         return "This field is required";
