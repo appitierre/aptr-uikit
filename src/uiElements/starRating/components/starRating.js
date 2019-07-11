090e7679a3a0ded6a5cfa6711612bd74543daade
Joe <joe.langley@appitierre.com>
2019-07-11 12:33:23 +0100

    Changes to Star Rating component
    Fixed autofocus so that it doesn't autofocus on payload, only when it is necessary to autofocus

--- src/uiElements/starRating/components/starRating.js
+++ src/uiElements/starRating/components/starRating.js
@@ -23,48 +23,48 @@ var StarRating = React.createClass({
 
 	getItems: function() {
 		var total = this.props.total;
+		var autoFocusProp = this.props.autoFocus;
 		var that = this;
 		var value = (this.state.hoveredItemNumber || this.props.value);
-		
+
 		return _.times(total, function(index) {
-			
-			var icon = "star-empty";	
-			var type = ""
+
 			var autoFocus = false;
-			
+			var icon = "star-empty";
+			var type = ""
 			if (index + 1 <= value) {
 				icon = "star";
 				type = "primary"
 			}
 
-			if (index === 0 ){
-				autoFocus = true;
+			if (index === 0 & autoFocusProp) {
+				autoFocus = true
 			}
 
 			var input = <input
-							type="radio"
-							value={index+1}
-							name="rating"
-							autoFocus={autoFocus}
-							disabled={that.props.isDisabled}
-							onClick={() => {that.onHover(index+1)}}
-							onKeyDown={(event) => {that.handleRadioButtonKeyPress(event, index+1)}}
-							id={`rate${index+1}`}
-						/>
-			
-			var labelIcon = <i className={`icon icon-${icon} ${type}`}/>
+				type="radio"
+				value={index + 1}
+				name="rating"
+				autoFocus={autoFocus}
+				disabled={that.props.isDisabled}
+				onClick={() => {that.onHover(index + 1)}}
+				onKeyDown={(event) => {that.handleRadioButtonKeyPress(event, index + 1)}}
+				id={`rate${index + 1}`}
+			/>
+
+			var labelIcon = <i className={`icon icon-${icon} ${type}`} />
 
 			var label = <label
-							className={`star-rating-label ${that.props.isDisabled ? 'is-disabled' : ''}`}
-							htmlFor={`rate${index+1}`}
-							value={index+1}
-							onClick={() => {that.onButtonClick(index+1)}}
-							onMouseEnter={() => {that.onHover(index+1)}}
-							onMouseLeave={() => {that.onHover(null)}}
-						>
-							{labelIcon}	
-						</label>
-			
+				className={`star-rating-label ${that.props.isDisabled ? 'is-disabled' : ''}`}
+				htmlFor={`rate${index + 1}`}
+				value={index + 1}
+				onClick={() => {that.onButtonClick(index + 1)}}
+				onMouseEnter={() => {that.onHover(index + 1)}}
+				onMouseLeave={() => {that.onHover(null)}}
+			>
+				{labelIcon}
+			</label>
+
 			return [input, label];
 		})
 	},
@@ -85,15 +85,15 @@ var StarRating = React.createClass({
 		})
 	},
 
-	onButtonClick: function(itemNumber) {	
+	onButtonClick: function(itemNumber) {
 		if (this.props.isDisabled) {
 			return
-		}	
+		}
 		this.props.onChange(itemNumber);
 	},
 
 	render: function() {
-		return ( 
+		return (
 			<form className={this.getClassName()} aria-label={`Average star rating ${this.props.value} out of ${this.props.total}`}>
 				{this.getItems()}
 			</form>
